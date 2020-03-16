c3622b7cda6fef0d4cdaf306f12ad63f1b1bca76
ashur-k <ashurkanwal@yahoo.com>
2020-03-16 16:31:07 +0000

    jquery event module added

--- js/myscript.js
+++ js/myscript.js
@@ -2,3 +2,8 @@ $(document).ready(function() {
     $("h2").addClass("underline");//underline all <h2> elements
     $("ul").addClass("border"); //adds a border to all lists 
 });
+
+var addtext = $("<p></p>").text("Lorem 20");
+$("body").append(addtext);
+
+$("h2").before("Some text after");
