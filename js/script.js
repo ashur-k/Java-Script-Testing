6e8d7f5c887dbbd7448fbf51f43b5f1c444518dd
ashur-k <ashurkanwal@yahoo.com>
2020-03-22 18:59:41 +0000

    JAva script functions

--- js/script.js
+++ js/script.js
@@ -21,3 +21,14 @@ $(document).ready(function(){
         
     })
 })
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200){
+        document.getElementById("data").innerHTML = this.responseText;
+    }
+};
+
+xhr.open("GET", "https://swapi.co/api/");
+xhr.send();
+
