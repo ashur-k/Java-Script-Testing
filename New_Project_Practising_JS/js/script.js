170085a0f4c15c895a5ac44d6de03b5934cb5386
ashur-k <ashurkanwal@yahoo.com>
2020-03-23 10:47:53 +0000

    practicing Javascript

---
+++ New_Project_Practising_JS/js/script.js
@@ -0,0 +1,13 @@
+/*I am practicing java script here I have produce output on web page with html h1 tag and I have added css into it*/
+document.write("<h1>I am h1 heading produced from java script</h1>");
+document.write("<h2 style= color:green;>I am h2 heading produced from java script and I have green color on my css</h1>");
+document.write("<div id=jsdiv><h6>I m h6 heading inside id div, my div border is black</h6></div>");
+
+//Practicing JS variables
+var num = 16; //number
+var name = "Ashur"; // string
+var flag = true; //boolean
+
+document.write("<div id=jsdiv><h6> I am number sixteen inside variable produced inside div = " + num + " :see I am cool</h6></div>");
+
+alert(name + " " + num + " " + flag); // sending all three values in 1 alert
