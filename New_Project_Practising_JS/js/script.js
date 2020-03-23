3139bd4723ebd21cb8dbe9e3f5d67664781181b2
ashur-k <ashurkanwal@yahoo.com>
2020-03-23 11:39:12 +0000

    added javascript theory comments

--- New_Project_Practising_JS/js/script.js
+++ New_Project_Practising_JS/js/script.js
@@ -11,3 +11,48 @@ var flag = true; //boolean
 document.write("<div id=jsdiv><h6> I am number sixteen inside variable produced inside div = " + num + " :see I am cool</h6></div>");
 
 alert(name + " " + num + " " + flag); // sending all three values in 1 alert
+
+/* Operators in Java script
+    Arithmetic oprators
+    Comparision operators
+    Logical or relational operators
+    Assignment operators
+    conditional operators or ternary operators
+    string operators
+    type operators
+    bitwise operators
+
+1. Arithmetic oprators: + is addition, - subtraction, * is mulitiplicaiton / is division
+         % is modulus or Remainder
+         ++ is to increment by speciifed vlaue
+         -- is to decrement by specified value 
+
+2.          Comparision operators to test the condition for true and false
+                <Given that x = 5>          
+         == is equalt to " x==8" returns false  
+                          "x == 5" returns true
+                          " x == "5" " returns true
+        
+        === equal value and equal type "x === 5" true
+                            "x === "5" " return fasle because value in inverted commas makes it string.
+                    != is not equal to "x!=8"
+
+                    !== os not equal to value or not equal type
+                    > is greater than, < is lesser than, >= is greater than and equal to
+                    <= is lesser than and equal to
+3. Logical or relational operators we generally use in logic statlement like if and else
+        && is and operator (x < 10 && y > 1) is true
+        || is or (x == 5 || y == 5)  is false
+        ! is not !(x==y) is true
+4. Assignment operators to assign values
+    = , example x = y, same as x = y
+    +=, example x +=y, same x = X + y
+    -=, example x-= y, same as x = x - y
+    and same with *, /, y, <<=, >>=, &=, |= **=
+
+5. Conditional operators Javascript also contains a conditional opertator that assigns a value to a variable based on some condition
+
+
+6. type o operators
+
+*/
