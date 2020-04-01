aafb5bcddf185416e5cdddd0f20cbe090485c9d7
ashur-k <ashurkanwal@yahoo.com>
2020-04-01 14:38:34 +0000

    form validation

--- New_Project_Practising_JS/js/script.js
+++ New_Project_Practising_JS/js/script.js
@@ -18,7 +18,7 @@ function fn2() {
 }
 // Taking Input from SelectBox in Javascript
 function fn3(){
-    var select =document.getElementById("select_box");
+    var select = document.getElementById("select_box");
         alert(select.options[select.selectedIndex].value);
 }
 //getElementByTagName method in JavaScript
@@ -35,3 +35,47 @@ function changeStyling(){
     
 
 }
+
+function changeStyling1(){
+
+    var element = document.getElementsByClassName("mypara");
+    for (var x=0;x<element.length;x++)
+    {
+        element[x].style.color = "red";
+    }
+
+}
+function setNewImage(){
+   document.getElementById("img_1").src = "images/2.png"
+}
+
+function setOldImage(){
+ document.getElementById("img_1").src = "images/1.jpg"
+}
+
+function validate(){
+    var username = document.getElementById("uname");
+    var password = document.getElementById("password");
+
+    if(username.value.trim() == "" )
+      {
+        alert("Blank Username");
+        username.style.border = "solid 3px red";
+         document.getElementById("lbluser1").style.visibility="visible";
+        return false;
+    } 
+    else if (password.value.trim() == ""){
+        alert("Blank Passowrd");
+        password.style.border = "solid 3px red";
+       document.getElementById("lbluser2").style.visibility="visible";
+        return false;
+    } 
+    else if(password.value.trim().length<5){
+        alert("Password should be more than 5 characters");
+        document.getElementById("lbluser2").style.visibility="visible";
+        return false;
+    }
+    else{
+        true;
+    }
+    }
